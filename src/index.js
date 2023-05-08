404d0f7645c138b0b1e414a35e58258b71c6b950
shweta <shwetasheladiya921@gmail.com>
2023-05-08 15:51:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Banner from './components/Banner/banner';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+ 
+    <Banner />,
+
+  document.getElementById('root')
+);
